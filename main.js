1c93ab76b5c79a53275c50b44a730be92025e188
Brenno Boechat <brennoboechat81@hotmail.com>
2024-02-06 15:32:30 -0300

    sistema de cadastro e login

--- main.js
+++ main.js
@@ -11,7 +11,61 @@ function showPassword(){
     }
 }
 
+function showPasswordLogin() {
+    var inputPass = document.getElementById('loginPassword');
+    var buttonShowPass = document.getElementById('button-password');
+
+    if (inputPass.type === "password") {
+        inputPass.setAttribute('type', 'text');
+        buttonShowPass.classList.replace('bi-eye', 'bi-eye-slash');
+    } else {
+        inputPass.setAttribute('type', 'password');
+        buttonShowPass.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+}
+
 
 var formSignin = document.querySelector('#signin')
 var formSignup = document.querySelector('#signin')
 var formSignin = document.querySelector('#signin')
+
+function signup() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    const token = generateToken();
+
+    localStorage.setItem('token', token);
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+
+    alert('Cadastro realizado com sucesso!');
+    
+    window.location.href = "./index.html";
+}
+
+function generateToken() {
+    return Math.random().toString(36).substr(2);
+}
+
+
+// ========================LOGIN========================
+
+
+
+function login() {
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const storedUsername = localStorage.getItem('name');
+    const storedPassword = localStorage.getItem('password');
+
+    if (username === storedUsername && password === storedPassword) {
+        alert('Login bem-sucedido!');
+        
+    } else {
+        alert('Falha no login!');
+    }
+}
